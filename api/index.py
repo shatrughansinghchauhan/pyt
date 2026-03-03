afd6653eceb8b14ca9f3cd3c59ae9c78e77ca47b
shatrughansinghchauhan <schauhan.lko1931@gmail.com>
2026-03-03 23:05:33 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,10 @@
-def handler(request):
-    print("hello")
-    return {
-        "statusCode": 200,
-        "body": "Check Vercel logs"
-    }
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
+
